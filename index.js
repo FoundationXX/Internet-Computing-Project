a3bfc5af6c9e252e86557c3acc0edddbc246f9eb
Mason <mkriegel@macalester.edu>
2014-12-11 16:44:05 -0600

    test

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var cool = require('cool-ascii-faces');
+var pg = require('pg');
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM main_table');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM main_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
