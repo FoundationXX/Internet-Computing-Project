9ef664d854ba1e74b780dd175b7f50fe98c0976f
Mason <mkriegel@macalester.edu>
2014-12-11 16:58:13 -0600

    word

--- index.js
+++ index.js
@@ -5,25 +5,14 @@ var pg = require('pg');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM main_table');
+// pg.connect(process.env.DATABASE_URL, function(err, client) {
+//   var query = client.query('SELECT * FROM main_table');
 
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
-});
+//   query.on('row', function(row) {
+//     console.log(JSON.stringify(row));
+//   });
+// });
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM main_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
-});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
