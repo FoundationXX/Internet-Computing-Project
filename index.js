5123c1e50bdd38e8508c005aec55907e6c50c0df
Mason <mkriegel@macalester.edu>
2014-12-12 15:52:13 -0600

    word

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('index.html');
 })
 
 
